fe53aeab768f7f849f1051c7fc1b40c9100f117f
Mr. Clark <geoff@clarktribegames.com>
2022-10-25 04:23:16 +0000

    updated license.txt

--- src/tools/startup.js
+++ src/tools/startup.js
@@ -5,8 +5,12 @@ const {
 const startupChecks = async () => {
     const env = fileCheck('./.env')
     const nm = fileCheck('./node_modules')
+    const db = fileCheck('./db')
+    const defdb = fileCheck('./db/default.limit')
     console.log('Env:',env)
     console.log('Modules',nm)
+    console.log('DB Folder',db)
+    console.log('Default DB',defdb)
 }
 
 exports.startupChecks = startupChecks
